3a909dd25459174a725c144027d13d626210c9ad
victormongare1 <vicmongz254@gmail.com>
2020-04-04 23:01:44 +0300

    moved access token to environment file

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -7,13 +7,13 @@ import {environment} from '../environments/environment'
   providedIn: 'root'
 })
 export class HttpService {
-
+  apiURL="https://api.github.com/users/"
   user : User
   
 
 
   constructor(private http:HttpClient) { 
-    this.user= new User("","","")
+    this.user= new User("","","","","","","",new Date())
   }
   userRequest(input){
     var username = input
@@ -21,13 +21,23 @@ export class HttpService {
     avatar_url : any;
     name :any;
     bio:any;
+    public_repos:any;
+    followers:any;
+    following : any;
+    company : any;
+    updated_at: any
   }
 
   let promise = new Promise((resolve,reject)=>{
-    this.http.get<ApiResponse>(environment.apiURL+ username).toPromise().then(response=>{
+    this.http.get<ApiResponse>(this.apiURL+ username +'?access_token=' + environment.token).toPromise().then(response=>{
       this.user.avatar= response.avatar_url
       this.user.name=response.name
       this.user.bio=response.bio
+      this.user.repositories=response.public_repos
+      this.user.followers=response.followers
+      this.user.following=response.following
+      this.user.company=response.company
+      this.user.update=response.updated_at
       console.log(response.name)
 
       resolve()
@@ -35,7 +45,12 @@ export class HttpService {
     error=>{
       this.user.name="Victor Mongare"
       this.user.bio="Software developer"
-
+      this.user.avatar="https://avatars3.githubusercontent.com/u/60592312?s=400&v=4"
+      this.user.repositories="44"
+      this.user.followers="0"
+      this.user.following="0"
+      this.user.company="Moringa School"
+      this.user.update=new Date(2020,2,2)
       reject(error)
     })
   })
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,5 +1,5 @@
 export class User {
-  constructor(public avatar:any,public name: any,public bio:any){
+  constructor(public avatar:any,public name: any,public bio:any,public repositories : any,public followers: any,public following: any,public company:any,public update: Date){
 
   }
 }
