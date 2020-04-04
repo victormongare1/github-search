3cfcc4cb50ff811c2f779b645c39d4f50e9db1b3
victormongare1 <vicmongz254@gmail.com>
2020-04-04 12:24:42 +0300

    updated custom routing module

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule,Routes} from '@angular/router'
+import { UserComponent} from '../user/user.component';
+import { RepositoryComponent} from '../repository/repository.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
+import {AppComponent} from '../app.component';
 
-
+const routes:Routes=[
+  {path:"users",component:UserComponent},
+  {path:"repo",component:RepositoryComponent},
+  {path:"",component: AppComponent},
+  {path:'**',component:NotFoundComponent}
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
   ]
 })
 export class RoutingModule { }
