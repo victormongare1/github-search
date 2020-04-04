a70141b01b4ac32f7d8b91791a38520e3fd173a0
victormongare1 <vicmongz254@gmail.com>
2020-04-04 23:04:47 +0300

    listed environment.ts in git ignore file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: false,
-  apiURL:"https://api.github.com/users/",
   token:'271c4408d6ab79500cd3e8f9525995c180ac7796',
-  api :"https://api.github.com/users/?access_token=271c4408d6ab79500cd3e8f9525995c180ac7796"
+  
 };
 
 
