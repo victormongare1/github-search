02347911da76851a3225decd069ee81cd0a7604b
victormongare1 <vicmongz254@gmail.com>
2020-04-04 11:57:18 +0300

    created components for user and not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
